57b82aec935fbe2b9ebe46605233a2a8771939cd
jfaduna <flomeraduna@gmail.com>
2023-11-22 10:36:34 +1100

    add: added farm feature with database relationship

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override')
 
 const Product = require('./models/product')
+const Farm = require('./models/farm')
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(() => {
@@ -20,6 +21,66 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+// Farm Routes
+
+app.get('/farms', async (req, res) => {
+    const farms = await Farm.find({})
+    res.render('farms/index', { farms })
+})
+
+app.get('/farms/new', (req, res) => {
+    res.render('farms/new')
+})
+
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id).populate('products')
+    res.render('farms/show', { farm })
+})
+
+app.get('/farms/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const farm = await Farm.findById(id)
+    res.render('farms/edit', { farm })
+})
+
+app.put('/farms/:id', async (req, res) => {
+    const { id } = req.params
+    const farm = await Farm.findByIdAndUpdate(id, req.body)
+    res.redirect(`/farms/${farm._id}`)
+})
+
+app.post('/farms', async (req, res) => {
+    const farm = new Farm(req.body)
+    await farm.save()
+    res.redirect('/farms')
+})
+
+app.delete('/farms/:id', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findByIdAndDelete(id)
+    console.log(`Farm: ${farm.name} deleted`)
+    res.redirect('/farms')
+})
+
+app.get('/farms/:id/products/new', async (req, res) => {
+    const { id } = req.params
+    const farm = await Farm.findById(id)
+    res.render('products/new', { categories, farm })
+})
+
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params
+    const farm = await Farm.findById(id)
+    const { name, price, category } = req.body
+    const product = new Product({ name, price, category })
+    farm.products.push(product)
+    product.farm = farm
+    await farm.save()
+    await product.save()
+    res.redirect(`/farms/${farm._id}`)
+})
+
+// Product Routes
 const categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async (req, res) => {
@@ -46,7 +107,7 @@ app.post('/products', async (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
-    const product = await Product.findById(id);
+    const product = await Product.findById(id).populate('farm', 'name');
     res.render('products/show', { product })
 })
 
@@ -65,6 +126,7 @@ app.put('/products/:id', async (req, res) => {
 app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndDelete(id)
+    console.log(`Product: ${product.name} deleted`)
     res.redirect('/products')
 })
 
--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const productSchema = new mongoose.Schema({
+const productSchema = new Schema({
     name: {
         type: String,
         required: true
@@ -14,9 +15,14 @@ const productSchema = new mongoose.Schema({
         type: String,
         lowercase: true,
         enum: ['fruit', 'vegetable', 'dairy']
+    },
+    farm: {
+        type: Schema.Types.ObjectId,
+        ref: 'Farm'
     }
-})
+});
+
 
 const Product = mongoose.model('Product', productSchema);
 
-module.exports = Product;
+module.exports = Product; 
